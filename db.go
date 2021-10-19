374afc65934fc9dba8dbe9ce83a9485f7f2ade05
ruibing1 <ruibing187@proton.me>
2021-10-19 15:31:01 +0000

    change logo size

--- db.go
+++ db.go
@@ -5,3 +5,16 @@ import (
 	"github.com/rs/zerolog/log"
 
 	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	dbFile = "chats.db"
+	DB     *gorm.DB
+)
+
+type Message struct {
+	gorm.Model
+	ID      uint   `gorm:"primaryKey" json:"id"`
+	ChatID  string `json:"chatId,omitempty"`  // telegrams conversation id
