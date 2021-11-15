421a58f7d117ed6aea72bc4c0c08ca9734231e26
ruibing1 <ruibing187@proton.me>
2021-11-15 10:39:13 +0000

    Fix name error

--- main.go
+++ main.go
@@ -11,4 +11,5 @@ import (
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"github.com/joho/godotenv"
-	"github.com/rakyll/openai-g
+	"github.com/rakyll/openai-go"
+	"github.co
