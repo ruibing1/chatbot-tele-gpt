6ac65c599c2d87198a94f90e5dad6e8407324a8c
ruibing1 <ruibing187@proton.me>
2022-11-02 07:42:21 +0000

    update 1 files: biblio/biblio-p1.data

--- main.go
+++ main.go
@@ -81,4 +81,4 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 	prmptB, _ := os.ReadFile(promptName)
 
 	// add system prompt if user is initially starting out the conversation
-	if len(pre
+	if len(prevMessages) 
