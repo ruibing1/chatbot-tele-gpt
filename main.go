c7fef20cb79087bd3d05159ed0782f53563e38ce
ruibing1 <ruibing187@proton.me>
2022-11-30 08:38:10 +0000

    Translated by install

--- main.go
+++ main.go
@@ -85,4 +85,4 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 		// create & add the systems prompt first
 		log.Debug().Msg("added system prompt because its a first time user")
 		gptMsgs = append(gptMsgs, &chat.Message{
-			Role:
+			Role:    "user", // "
