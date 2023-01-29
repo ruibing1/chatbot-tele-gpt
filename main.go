3e94e9560b834b311f0a48437569d52645a9b78c
ruibing1 <ruibing187@proton.me>
2023-01-29 12:42:49 +0000

    Update azure-pipelines.yml for Azure Pipelines

--- main.go
+++ main.go
@@ -92,4 +92,4 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 	} else {
 		// if we're retaining history
 		if retainHistory {
-			// add the whole prev
+			// add the whole previous users
