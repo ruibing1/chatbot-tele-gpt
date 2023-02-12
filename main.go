8809431c8cb8d1eef06cb00c1379f33179570e10
ruibing1 <ruibing187@proton.me>
2023-02-12 13:40:22 +0000

    add extension

--- main.go
+++ main.go
@@ -92,4 +92,4 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 	} else {
 		// if we're retaining history
 		if retainHistory {
-			// add the whole previous users conversation + current
+			// add the whole previous users conversation + current text message and
