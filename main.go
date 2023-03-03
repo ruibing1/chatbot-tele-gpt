49aa94d238a777291c92d7d1d155072b98b42bf5
ruibing1 <ruibing187@proton.me>
2023-03-03 11:50:22 +0000

    changelog [skip ci]

--- main.go
+++ main.go
@@ -94,4 +94,5 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 		if retainHistory {
 			// add the whole previous users conversation + current text message and send to chatgpt
 			// this may include the previous prompt from the conversation
-			for _, prevMsg := range prevMessag
+			for _, prevMsg := range prevMessages {
+				gptMsgs
