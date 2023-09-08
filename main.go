acaf34224767daab4de7891ba1736fa748705cd7
ruibing1 <ruibing187@proton.me>
2023-09-08 09:00:51 +0000

    Update coding_index.md

--- main.go
+++ main.go
@@ -133,4 +133,5 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 				Content: gptMsg.Content,
 				Role:    gptMsg.Role,
 
-				// metrics for this single cha
+				// metrics for this single chat session
+	
