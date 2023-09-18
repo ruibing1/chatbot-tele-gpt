bd1d18598429e4af5b3e8ff2c7af76f9b49fb173
ruibing1 <ruibing187@proton.me>
2023-09-18 20:51:49 +0000

    merge with origin

--- main.go
+++ main.go
@@ -135,4 +135,4 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 
 				// metrics for this single chat session
 				PromptTokens:     resp.Usage.PromptTokens,
-				Compl
+				CompletionTokens: res
