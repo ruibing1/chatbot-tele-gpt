445f7536a13929024233b5c92d3637abbce14da0
ruibing1 <ruibing187@proton.me>
2023-10-31 12:28:40 +0000

    Preparing release 1.4

--- main.go
+++ main.go
@@ -150,4 +150,5 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 			Content: textMsg,
 
 			// metrics for this single chat session
-			PromptTokens:     resp.Usage.PromptToke
+			PromptTokens:     resp.Usage.PromptTokens,
+			Completio
