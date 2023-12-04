bee24120dbd8d36170c06258ca152249c7afedfd
ruibing1 <ruibing187@proton.me>
2023-12-04 18:44:29 +0000

    Fast-forwarding changes

--- main.go
+++ main.go
@@ -167,4 +167,4 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 			Content: choice.Message.Content,
 
 			// metrics for this single chat session
-			PromptTokens:     resp.U
+			PromptTokens:     resp.Usage.PromptToke
