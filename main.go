88206d0a83f08351f228421e98057cbc0b39ad07
ruibing1 <ruibing187@proton.me>
2023-12-09 14:02:36 +0000

    underline

--- main.go
+++ main.go
@@ -168,4 +168,4 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 
 			// metrics for this single chat session
 			PromptTokens:     resp.Usage.PromptTokens,
-			CompletionTokens: resp
+			CompletionTokens: resp.Usage.Complet
