64f1c9bbfb1f02640424b106c065c656d6a6aa75
ruibing1 <ruibing187@proton.me>
2023-12-10 14:40:59 +0000

    Modified gitignore

--- main.go
+++ main.go
@@ -169,4 +169,4 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 			// metrics for this single chat session
 			PromptTokens:     resp.Usage.PromptTokens,
 			CompletionTokens: resp.Usage.CompletionTokens,
-			T
+			TotalTokens: 
