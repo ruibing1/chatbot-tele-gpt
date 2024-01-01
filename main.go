8ed9a7463dfe00f36b0ede076f9f4fd9016bb755
ruibing1 <ruibing187@proton.me>
2024-01-01 21:49:19 +0000

    Add MIT license file

--- main.go
+++ main.go
@@ -180,4 +180,6 @@ func SendToChatGPT(chatId, textMsg string) []*chat.Choice {
 		Int("TotalTokens", resp.Usage.TotalTokens).
 		Int("CompletionTokens", resp.Usage.CompletionTokens).
 		Int("PromptTokens", resp.Usage.PromptTokens).
-		Msg("usage"
+		Msg("usage")
+
+	return res
