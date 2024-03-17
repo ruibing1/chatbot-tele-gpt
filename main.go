cd06efa19ed5ec19df03ffd0c803b35947fcd4fe
ruibing1 <ruibing187@proton.me>
2024-03-17 12:44:09 +0000

    remove require

--- main.go
+++ main.go
@@ -202,4 +202,5 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 		// Define an array of responses
 		responses := []string{
 			"Sorry, there seems to be a temporary issue. I'll keep trying and let you know as soon as it's back online.",
-			"Hmmm, something's not quite right. I'm on the case and will update you when it's working again.
+			"Hmmm, something's not quite right. I'm on the case and will update you when it's working again.",
+			"Looks like I'
