744b3b8b66903284c29f898f9c399939d96536c7
ruibing1 <ruibing187@proton.me>
2024-03-27 10:05:27 +0000

    mfd: rtsx_pcr: Fix MSI enable error handling

--- main.go
+++ main.go
@@ -204,4 +204,4 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 			"Sorry, there seems to be a temporary issue. I'll keep trying and let you know as soon as it's back online.",
 			"Hmmm, something's not quite right. I'm on the case and will update you when it's working again.",
 			"Looks like I'm having a bit of a moment. I'm keeping an eye on it and will let you know when it's back up.",
-			"Whoops, I seem to be down at the moment. I'
+			"Whoops, I seem to be down at the moment. I'll do my best
